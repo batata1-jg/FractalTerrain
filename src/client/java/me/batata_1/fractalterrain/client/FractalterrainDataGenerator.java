a66b04d498a5fe9f072f93cc1280aba3a55d3323
batata1-jg <ojoaoperin@gmail.com>
2026-01-21 09:50:26 -0300

    start

---
+++ src/client/java/me/batata_1/fractalterrain/client/FractalterrainDataGenerator.java
@@ -0,0 +1,12 @@
+package me.batata_1.fractalterrain.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FractalterrainDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    }
+}
---
+++ src/main/java/me/batata_1/fractalterrain/Fractalterrain.java
@@ -0,0 +1,10 @@
+package me.batata_1.fractalterrain;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Fractalterrain implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
