0f3ada4eb1ec4001c3360c2cc4fd9ae82a6128b2
batata1-jg <ojoaoperin@gmail.com>
2026-01-21 10:15:42 -0300

    add density function

--- src/main/java/me/batata_1/fractalterrain/FractalTerrain.java
+++ src/main/java/me/batata_1/fractalterrain/FractalTerrain.java
@@ -2,9 +2,11 @@ package me.batata_1.fractalterrain;
 
 import net.fabricmc.api.ModInitializer;
 
-public class Fractalterrain implements ModInitializer {
+public class FractalTerrain implements ModInitializer {
 
     @Override
     public void onInitialize() {
+
+
     }
 }
---
+++ src/main/java/me/batata_1/fractalterrain/world/gen/FractalTerrainDensityFunction.java
@@ -0,0 +1,4 @@
+package me.batata_1.fractalterrain.world.gen;
+
+public class FractalTerrainDensityFunction {
+}
